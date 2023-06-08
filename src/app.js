3cf2869fc040f1519105ce3ff0d50469f070befa
Romina Verónica Torres <110548507+RomiTorres@users.noreply.github.com>
2023-06-08 09:29:05 +0000

    Vainillajs basico

--- src/app.js
+++ src/app.js
@@ -8,4 +8,40 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  // declaracion de variables
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function aleatorio(who, action, what, when) {
+    let whonum = Math.floor(Math.random() * who.length);
+    let actionnum = Math.floor(Math.random() * action.length);
+    let whatnum = Math.floor(Math.random() * what.length);
+    let whennum = Math.floor(Math.random() * when.length);
+    return (
+      who[whonum] +
+      " " +
+      action[actionnum] +
+      " " +
+      what[whatnum] +
+      " " +
+      when[whennum]
+    );
+  }
+  document.querySelector("#excuse").innerHTML = aleatorio(
+    who,
+    action,
+    what,
+    when
+  );
+  // function onLoad() {}
+  console.log("Hello Rigo from the console!");
 };
